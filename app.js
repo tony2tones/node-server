f1142cfd0c8b2a758c75bb998bcdb15f670d252e
Anthony <anthonymarques@outlook.com>
2019-10-12 14:38:05 +0200

    add styl sheets and
    updated app.js to include stylesheets root

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(( req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views','page-not-found.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views','404.html'));
 })
 
 
